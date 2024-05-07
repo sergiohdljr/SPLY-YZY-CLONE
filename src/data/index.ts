9a6be7f31db0a419a2df70351f6d8cbe0acdb431
sergiohdljr <sergio.tjf1@gmail.com>
2024-05-07 18:34:55 -0300

    feat: product data dummy

---
+++ src/data/index.ts
@@ -0,0 +1,16 @@
+const products = [
+  {
+    name: "HEATHER GREY TEE",
+    photos: [
+      "https://yeezy.com/cdn/shop/files/1_1e5b5542-64f6-4828-92de-9e107da57dfa.png?height=650&v=1714270021",
+      "https://yeezy.com/cdn/shop/files/2_709010a9-0df8-4ee7-b102-b7601e874397.png?height=650&v=1714270020",
+      "https://yeezy.com/cdn/shop/files/3_6ace5d5f-bff5-46c3-a3e9-74a9bf439bf5.png?height=650&v=1714270017",
+      "https://yeezy.com/cdn/shop/files/4_1c800a35-7d95-4550-a4ba-7c080ce3a9c9.png?height=650&v=1714270019",
+      "https://yeezy.com/cdn/shop/files/5_6a624346-d609-46d6-ae27-7025ee32f0bb.png?height=650&v=1714270022",
+      "https://yeezy.com/cdn/shop/files/6_95da2f4f-e829-40f2-90a0-7a49a2bdf796.png?height=650&v=1714270020",
+      "https://yeezy.com/cdn/shop/files/7_c9f3dcf3-c4e6-4d80-84b7-c3dae4919f66.png?height=650&v=1714270017",
+      "https://yeezy.com/cdn/shop/files/8_cac211c4-3026-4eea-bb07-8082ed258659.png?height=650&v=1714270020",
+    ],
+    price:"$20"
+  },
+];
