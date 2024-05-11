7333175e21133f19ae5031e2f56e642013c49dd6
sergiohdljr <sergio.tjf1@gmail.com>
2024-05-11 11:00:36 -0300

    feat: styles container main

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,18 @@
 <script lang="ts">
-  
+  import ProductCard from "./lib/product-card.svelte";
 </script>
 
-<main>
- 
+<main class="container">
+  <ProductCard />
 </main>
 
 <style>
-  
+  .container {
+    width: 100%;
+    height: 100vh;
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
